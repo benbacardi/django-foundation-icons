8b430af87b6916cd946620eb2b352ea89ca64f51
Ben Cardy <ben.cardy@ocado.com>
2014-03-07 15:14:01 +0000

    Added scss file too

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-foundation-icons',
-    version='3',
+    version='3.1',
     url='https://github.com/benbacardi/django-foundation-icons',
     description='Zurb Foundation Icons (http://zurb.com/playground/foundation-icon-fonts-3) static files packaged in a Django app.',
     author='Ben Cardy',
